9553c1b648e8f99869424707bace520fcf029bd8
JackWils91 <jackdavidwilson@gmail.com>
2019-06-03 14:56:29 +0100

    completes migrations

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,10 +1,21 @@
 // const {  } = require('../data');
+const { topicData, userData, articleData } = require("../data/index");
 
 exports.seed = (knex, Promise) => {
   return knex.migrate
     .rollback()
     .then(() => knex.migrate.latest())
     .then(() => {
-      // insert data
+      return knex("topics")
+        .insert(topicData)
+        .returning("*");
+    })
+    .then(() => {
+      return knex("users")
+        .insert(userData)
+        .returning("*");
+    })
+    .then(() => {
+      // need to take userData, pass it  through a function and add it author into autor key
     });
 };
